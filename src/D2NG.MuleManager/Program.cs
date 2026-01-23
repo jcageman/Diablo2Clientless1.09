ca3f0f1e1c7e10ce5596b0473e1fc7f11753bc8b
jacob kooijman <j.cageman@gmail.com>
2026-01-23 12:35:15 +0100

    Switch to Program.cs new .NET style

--- src/D2NG.MuleManager/Program.cs
+++ src/D2NG.MuleManager/Program.cs
@@ -1,19 +1,55 @@
-using Microsoft.AspNetCore.Hosting;
+using D2NG.MuleManager.Configuration;
+using D2NG.MuleManager.Services.MuleManager;
+using Marten;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
-namespace D2NG.MuleManager;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args: args);
+
+var configuration = builder.Configuration;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .MinimumLevel.Override("System.Net.Http.HttpClient", LogEventLevel.Warning)
+    .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+    .CreateLogger();
+
+builder.Logging.AddSerilog();
+
+builder.Services.AddControllers();
+builder.Services.AddMarten(configuration.GetConnectionString("Marten"));
+builder.Services.AddOptions<MuleManagerConfiguration>()
+    .Bind(configuration.GetSection("mulemanager"))
+    .ValidateDataAnnotations();
+builder.Services.AddScoped<IMuleManagerRepository, MuleManagerRepository>();
+builder.Services.AddScoped<IMuleManagerService, MuleManagerService>();
+builder.Services.AddCors(options =>
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    options.AddPolicy("CorsPolicy",
+        policy => policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseCors("CorsPolicy");
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
